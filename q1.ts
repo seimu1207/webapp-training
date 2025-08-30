ff8cff9be0e9ad8a29fd8279c541d9e59ab5b102
seimu1207 <74397162+seimu1207@users.noreply.github.com>
2025-08-30 10:08:36 +0900

    Create q1.ts

---
+++ q1.ts
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TypeScript test</title>
+    <style>
+        #dog, #cat {
+            margin-bottom: 20px;
+            margin-top: 20px;
+            margin-right: 20px;
+            margin-left: 10px;
+            font-size: 30px
+        }
+        #reset{
+          border-radius: 20px;
+          font-size: 20px;
+        }
+        #message{
+          color: red;
+          font-size: 18px;
+          margin-top: 10px;
+        }
+    </style>
+  </head>
+  <body>
+    <button id="dog">dog</button>
+    <button id="cat">cat</button>
+    <button id="reset"">reset</button>
+    <div id="chirp">鳴き声</div>
+    <div id="message"></div>
+    <script src="q1.js"></script>
+  </body>
+</html>
